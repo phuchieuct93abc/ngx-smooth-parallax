291c79ff71bc4df41154e2c4bcb7391366db0e43
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2023-01-08 18:06:15 +0700

    request animation

--- projects/ngx-smooth-parallax/src/lib/ngx-smooth-parallax.module.ts
+++ projects/ngx-smooth-parallax/src/lib/ngx-smooth-parallax.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
-import { ParallaxDirective } from './parallax.directive';
 import { ParallaxComponent } from './parallax/parallax.component';
 
 @NgModule({
   declarations: [
-    ParallaxDirective,
     ParallaxComponent
-
-  ],
-  imports: [
-    
   ],
   exports: [
-    ParallaxDirective,
     ParallaxComponent
   ]
 })
--- projects/ngx-smooth-parallax/src/public-api.ts
+++ projects/ngx-smooth-parallax/src/public-api.ts
@@ -1,7 +1,4 @@
 /*
  * Public API Surface of ngx-smooth-parallax
  */
-
-export * from './lib/parallax.directive';
-export * from './lib/parallax.directive';
 export * from './lib/parallax/parallax.component';
